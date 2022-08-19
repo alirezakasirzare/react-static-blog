252ef453ade0bf86a124a5f18130f51c4093278e
alireza kasirzare <alirezakasir@yahoo.com>
2022-08-19 10:41:14 +0430

    fix navbar responsive

--- src/components/public/Header.jsx
+++ src/components/public/Header.jsx
@@ -27,23 +27,34 @@ function Header() {
 
   return (
     <header className="mb-3">
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-sm bg-light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             React blog
           </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mobile-navbar"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
           {/* navigation links */}
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {navRoutes.map((route, index) => {
-              return (
-                <li className="nav-item" key={index}>
-                  <NavLink className="nav-link" to={route.path}>
-                    {route.text}
-                  </NavLink>
-                </li>
-              );
-            })}
-          </ul>
+          <div class="collapse navbar-collapse" id="mobile-navbar">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {navRoutes.map((route, index) => {
+                return (
+                  <li className="nav-item" key={index}>
+                    <NavLink className="nav-link" to={route.path}>
+                      {route.text}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </nav>
     </header>
